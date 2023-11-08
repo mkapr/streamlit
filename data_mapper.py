222b7eaca0184122ebd1f491fafc7934b651e26e
mkapr <mkapr@users.noreply.github.com>
2023-11-08 12:30:10 +1100

    Create data_mapper.py

---
+++ data_mapper.py
@@ -0,0 +1,63 @@
+import streamlit as st
+import snowflake.connector
+
+# Function to establish a Snowflake connection
+def create_snowflake_connection():
+    conn = snowflake.connector.connect(
+        user=st.secrets["snowflake_username"],
+        password=st.secrets["snowflake_password"],
+        account=st.secrets["snowflake_account"],
+        warehouse=st.secrets["snowflake_warehouse"],
+        database=st.secrets["snowflake_database"],
+        schema=st.secrets["snowflake_schema"]
+    )
+    return conn
+
+# Streamlit app
+st.title("Data Mapping Wizard with Snowflake")
+
+# Step 1: Snowflake Connection
+st.header("Step 1: Connect to Snowflake")
+
+if "snowflake_connection" not in st.session_state:
+    st.session_state.snowflake_connection = None
+
+if st.button("Connect to Snowflake"):
+    st.session_state.snowflake_connection = create_snowflake_connection()
+    st.success("Connected to Snowflake!")
+
+# Step 2: Select Source and Target Data
+
+if st.session_state.snowflake_connection:
+    st.header("Step 2: Select Source and Target Data")
+
+    with st.beta_container():
+        with st.form("source_target_form"):
+            st.subheader("Source Data:")
+            source_schema = st.selectbox("Select Source Schema", ["schema_name_1", "schema_name_2"])
+            source_query = f"SHOW TABLES IN SCHEMA {source_schema}"
+
+            with st.spinner("Loading source tables..."):
+                source_cursor = st.session_state.snowflake_connection.cursor()
+                source_cursor.execute(source_query)
+                source_tables = [row[1] for row in source_cursor]
+
+            source_table = st.selectbox("Select Source Table", source_tables)
+
+            st.subheader("Target Data:")
+            target_schema = st.selectbox("Select Target Schema", ["schema_name_a", "schema_name_b"])
+            target_query = f"SHOW TABLES IN SCHEMA {target_schema}"
+
+            with st.spinner("Loading target tables..."):
+                target_cursor = st.session_state.snowflake_connection.cursor()
+                target_cursor.execute(target_query)
+                target_tables = [row[1] for row in target_cursor]
+
+            target_table = st.selectbox("Select Target Table", target_tables)
+
+            submit_button = st.form_submit_button("Submit")
+
+        if submit_button:
+            st.success("Source and Target data selected!")
+
+# Continue with the rest of the data mapping wizard steps.
